a7c0b0b82783ce4e14e96c8082f461966dbfabe3
Reinald Reynoso <reireynoso@gmail.com>
2019-03-25 01:00:16 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |word|
+    word.split("").map do |letter|
+      #puts letter
+      esperanto_alphabet.index(letter)
+    end
+  end
 end
