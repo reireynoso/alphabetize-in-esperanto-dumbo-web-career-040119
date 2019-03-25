82e3d26a2a42509ff7277f31d751acc9c483f6f6
Reinald Reynoso <reireynoso@gmail.com>
2019-03-25 01:00:10 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,7 @@ def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   arr.sort_by do |word|
     word.split("").map do |letter|
-      puts letter
+      #puts letter
       esperanto_alphabet.index(letter)
     end
   end
